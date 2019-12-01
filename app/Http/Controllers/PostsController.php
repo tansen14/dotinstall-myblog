905ce8510cd1af26a5475dd1f12e4be840713e99
vagrant <vagrant@localhost.localdomain>
2019-12-01 11:39:44 +0900

    first commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostsController extends Controller
+{
+    //
+    public function index(){
+        // $posts = Post::all();
+        $posts = Post::latest()->get();
+        // dd($posts->toArray());
+        return view('posts.index')->with('posts', $posts);
+    }
+}
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog Posts</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Blog Posts</h1>
+            <ul>
+                @foreach ($posts as $post)
+                <li><a href="">title</a></li>
+                @endforeach
+            </ul>
+    </body>
+</html>
